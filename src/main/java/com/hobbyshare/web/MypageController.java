96f5eae71b75872bb7f30d845146cd2ce2d423cb
shin <s_swwhh@naver.com>
2019-12-24 20:34:00 +0900

    mypage form추가

---
+++ src/main/java/com/hobbyshare/web/MypageController.java
@@ -0,0 +1,15 @@
+package com.hobbyshare.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mypage")
+public class MypageController {
+	
+	@GetMapping("form")
+	public void form() {
+		
+	}
+}
